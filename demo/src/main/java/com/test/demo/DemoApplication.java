2e2cf4a69ea5358fd56da15fce3349e010f8322f
PRINCESS LEYSON <63419-22229348@clab203.com>
2024-07-10 12:12:52 +0800

    first commit

---
+++ demo/src/main/java/com/test/demo/DemoApplication.java
@@ -0,0 +1,13 @@
+package com.test.telecprides;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class telecpridesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(telecpridesApplication.class, args);
+	}
+
+}
